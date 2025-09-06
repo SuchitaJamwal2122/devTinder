94d23d2ed5b8b60d718b37d33a9e670df6c2b317
Suchita Jamwal <suchita7j@gmail.com>
2025-09-06 21:16:40 +0530

    middleware and error handling

--- src/app.js
+++ src/app.js
@@ -33,57 +33,57 @@ connectDB()
     console.error("Database cannot be connected!!");
   });
 
-// app.get("/admin/getAllData", (req, res) => {
-//   // Logic to Check if the request is authorised
-//   // Logic of fetching all data
-//   const token = "xyz1"; //req.body?.token;
-//   const isAdminAuthorised = token === "xyz";
-//   isAdminAuthorised
-//     ? res.send("All Data sent")
-//     : res.status(401).send("Unauthorised request");
-// });
+app.get("/admin/getAllData", (req, res) => {
+  // Logic to Check if the request is authorised
+  // Logic of fetching all data
+  const token = "xyz1"; //req.body?.token;
+  const isAdminAuthorised = token === "xyz";
+  isAdminAuthorised
+    ? res.send("All Data sent")
+    : res.status(401).send("Unauthorised request");
+});
 
-// app.get("/admin/deleteUser", (req, res) => {
-//   // Logic to delete a user
+app.get("/admin/deleteUser", (req, res) => {
+  // Logic to delete a user
 
-//   res.send("Deleted a user");
-// });
+  res.send("Deleted a user");
+});
 
-// app.use("/", (err, req, res, next) => {
-//   if (err) {
-//     // Log your error
-//     res.status(500).send("Something went wrong");
-//   }
-// });
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    // Log your error
+    res.status(500).send("Something went wrong");
+  }
+});
 
-// app.use(
-//   "/user",
-//   (req, res, next) => {
-//     //Route Handler
+app.use(
+  "/user",
+  (req, res, next) => {
+    //Route Handler
 
-//     next();
-//     res.send("Route Handler 1");
-//   },
-//   (req, res) => {
-//     res.send("Route Handler 2");
-//   }
-// );
+    next();
+    res.send("Route Handler 1");
+  },
+  (req, res) => {
+    res.send("Route Handler 2");
+  }
+);
 
-// // This will handle only GET API call to /user
-// app.get("/user", (req, res) => {
-//   res.send({ firstname: "Suchita", lastname: "Jamwal" });
-// });
+// This will handle only GET API call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Suchita", lastname: "Jamwal" });
+});
 
-// app.post("/user", (req, res) => {
-//   console.log("Save data to the database");
-//   res.send("Data successfully saved to the database");
-// });
+app.post("/user", (req, res) => {
+  console.log("Save data to the database");
+  res.send("Data successfully saved to the database");
+});
 
-// app.use("/test", (req, res) => {
-//   // app.use is a request handler
-//   res.send("Hello from the server");
-// });
+app.use("/test", (req, res) => {
+  // app.use is a request handler
+  res.send("Hello from the server");
+});
 
-// app.use("/", (req, res) => {
-//   res.send("Namaste Suchita");
-// });
+app.use("/", (req, res) => {
+  res.send("Namaste Suchita");
+});
