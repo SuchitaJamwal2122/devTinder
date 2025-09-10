d72e44a62b8b3a0956e99bb55c795b153f818f09
Suchita Jamwal <suchita7j@gmail.com>
2025-09-11 00:55:37 +0530

    Api to change /profile/password

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const profileRouter = express.Router();
 
@@ -23,6 +25,34 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     res.json({
       message: `${loggedInUser.firstName}, your profile was updated successfully.`,
       data: loggedInUser,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const { password, newPassword } = req.body;
+    const loggedInUser = req.user;
+
+    const isPasswordValid = await loggedInUser.validatePassword(password);
+    if (!isPasswordValid) {
+      throw new Error("Current password is not valid.");
+    }
+
+    if (!validator.isStrongPassword(newPassword)) {
+      throw new Error("Enter a strong password");
+    }
+
+    const newPasswordHash = await bcrypt.hash(newPassword, 10);
+    loggedInUser.password = newPasswordHash;
+
+    await loggedInUser.save();
+
+    res.json({
+      message: "Password updated successfully.",
+      data: newPasswordHash,
     });
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
